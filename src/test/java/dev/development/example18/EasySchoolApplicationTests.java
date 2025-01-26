9a45f7b60e91a404479d6e69ae2b655212570cb8
Devesh-Ingale <150885752+Devesh-Ingale@users.noreply.github.com>
2025-01-26 13:01:13 +0530

    initial commit

---
+++ src/test/java/dev/development/example18/EasySchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.development.example18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
